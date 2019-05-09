1397f53ee7e13ec34a83deefe0859b0d8f3a3b71
Kevin Wu <kevinwu@Kevins-MacBook-Air-4.local>
2019-05-09 16:21:22 -0400

    fix

--- cs441_assignment7/cs441_assignment7/DataViewController.m
+++ cs441_assignment7/cs441_assignment7/DataViewController.m
@@ -14,6 +14,10 @@
 
 @implementation DataViewController
 
+@synthesize label;
+@synthesize image;
+@synthesize toggle;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
